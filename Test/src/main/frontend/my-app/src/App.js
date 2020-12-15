1bf0f3d84cbe042f6fc9cf92ebfde70ccb91d63b
hyunjechoi <toyoly@naver.com>
2020-12-16 01:03:31 +0900

    init commi

---
+++ Test/src/main/frontend/my-app/src/App.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+import AuthService from "./services/auth.service";
+
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+import BoardModerator from "./components/board-moderator.component";
+import BoardAdmin from "./components/board-admin.component";
+import UploadFiles from "./components/upload-files.component";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+                TEST 사이트
+          </Link>
+
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+             <div className="navbar-nav mr-auto">
+                          <li className="nav-item">
+                            <Link to={"/UploadFiles"} className="nav-link">
+                              UploadFiles
+                            </Link>
+                          </li>
+                          </div>
+
+
+
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
+
+             {showModeratorBoard && (
+                          <li className="nav-item">
+                            <Link to={"/mod"} className="nav-link">
+                              Moderator Board
+                            </Link>
+                          </li>
+                        )}
+
+
+
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+
+        <div className="container mt-4">
+                  <Switch>
+                    <Route exact path={["/", "/home"]} component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route path="/user" component={BoardUser} />
+                    <Route path="/mod" component={BoardModerator} />
+                    <Route path="/admin" component={BoardAdmin} />
+                    <Route path="/UploadFiles" component={UploadFiles} />
+                  </Switch>
+                   </div>
+                    </div>
+
+        );
+          }
+        }
+export default App;
---
+++ Test/src/main/frontend/my-app/src/services/upload-files.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import http from "../http-common";
+const API_URL = "http://localhost:8080/api/upload/";
+
+class UploadFilesService {
+  upload(file, onUploadProgress) {
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return http.post("/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  }
+
+  getFiles() {
+    return http.get("/files");
+  }
+}
+
+export default new UploadFilesService();
